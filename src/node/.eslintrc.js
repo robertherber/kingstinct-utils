1592d4dab23003ea00c6e8cf917138b5feb56698
Robert Herber <robert@herber.me>
2020-01-11 17:08:26 +0100

    Fix shit

--- src/node/.eslintrc.js
+++ src/node/.eslintrc.js
@@ -2,10 +2,10 @@ const config = require('../../.eslintrc.js');
 
 config.parserOptions.project = './tsconfig.node.json';
 config.parserOptions.sourceType = 'module';
+config.parserOptions.features = { "modules": true };
 
 config.extends = [...config.extends, 'plugin:node/recommended'];
 config.env = { node: true, es6: true };
-
 
 config.rules['node/no-unsupported-features/es-syntax'] = 0;
 config.rules['node/no-missing-import'] = 0;
