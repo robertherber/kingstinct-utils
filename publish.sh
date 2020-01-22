26a298d2650d9753188e20c5e4e5f9d28375fe49
Robert Herber <robert@herber.me>
2020-01-22 01:39:00 +0100

    added 1p integration

--- publish.sh
+++ publish.sh
@@ -5,4 +5,6 @@ yarn version
 yarn build
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed -E 's/(version)|[:,\",]//g' | tr -d '[[:space:]]')
 echo $PACKAGE_VERSION
-yarn publish lib --new-version=$PACKAGE_VERSION
+if [ -z "$OP_SESSION_my" ]; then eval $(op signin my); fi # ask for 1Password login if not done
+export OTP=$(op get totp Npmjs)
+yarn publish lib --new-version=$PACKAGE_VERSION --otp $OTP
